e79a804253860ae955b00d4a1b6bdc9bb5f52348
Nandana <nandana.a2014@srisriuniversity.edu.in>
2014-11-23 15:42:00 +0530

    Plot4

---
+++ plot4.R
@@ -0,0 +1,45 @@
+#  Read the data
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Find which SCC codes correspond to Coal
+
+CoalCodes <- SCC[grep("Coal", SCC$EI.Sector),]["SCC"]
+
+#pull out(subset) just the coal codes
+
+JustCoal <- NEI [NEI$SCC %in% CoalCodes$SCC,]
+
+# Calculate the total coal emissions by year
+
+YearTotal <- aggregate (Emissions ~ year, data = JustCoal, sum)
+
+
+#open the graphics device
+
+png (file = "plot4.png",width = 480, height = 480, bg=NA)
+
+#create a line plot to show trend
+#turn off axes because base plot uses different years than the data
+
+plot (YearTotal$year, YearTotal$Emissions, type="l", axes=FALSE,
+      xlab="",ylab="Annual Emissions (Tons)", 
+      main="Total US Emissions from coal combustion-related sources from 1999-2008")
+
+#add characters showing the data points on graph
+
+points(YearTotal$year, YearTotal$Emissions, pch = 8, col = "red")
+
+#draw the axes
+
+axis(side = 1, at = seq(1999, 2008, by = 3))
+axis(side=2)
+
+#add the box around the plot to make it look pretty normal
+
+box ()
+
+#Turn off the graphics device
+
+dev.off ()
